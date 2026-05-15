4b76fbd153f0af15237f546643755db6c8570215
hrabstepann-png <hrabstepann@gmail.com>
2026-05-15 13:30:12 +0300

    Update app.py

--- app.py
+++ app.py
@@ -10,6 +10,8 @@ ASSISTANT_ID = st.secrets["ASSISTANT_ID"]
 client = openai.Client(api_key=OPENAI_API_KEY)
 
 st.set_page_config(page_title="База знань БП", page_icon="📚")
+# Відображення логотипу
+st.image("logo.png", width=150)
 # Приховуємо стандартні елементи інтерфейсу Streamlit (GitHub, меню, футер)
 hide_ui_style = """
 <style>
